cda7feb8beec4cd5e22bea79b042af8f450869a3
Dari <dariatsevashova@gmail.com>
2026-01-14 21:00:11 +0100

    remove redundant logging middleware

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 app.use(
   pino({
@@ -25,16 +25,11 @@ app.use(
     },
   }),
 );
-// Логування
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
 
 // Маршрути
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: '' });
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
 });
 
 app.get('/notes', (req, res) => {
@@ -49,7 +44,7 @@ app.get('/notes/:noteId', (req, res) => {
   });
 });
 
-// Middleware 404 (після всіх маршрутів)
+// Middleware 404
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
