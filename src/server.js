bb474c83daad55a2191d997ae301f45a14abca5f
Dari <dariatsevashova@gmail.com>
2026-01-26 11:19:23 +0100

    add crud operations for notes

--- src/server.js
+++ src/server.js
@@ -1,67 +1,35 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 import 'dotenv/config';
+
 import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-// Middleware
+// Глобальні middleware
+app.use(logger);
 app.use(express.json({ limit: '10mb' }));
 app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
 
 // Маршрути
+// підключаємо групу маршрутів для нотаток
+app.use(notesRoutes);
 
 app.get('/test-error', () => {
   throw new Error('Simulated server error');
 });
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
+// 404 — якщо маршрут не знайдено
+app.use(notFoundHandler);
 
-app.get('/notes/:noteId', (req, res) => {
-  res.status(200).json({
-    message: `Retrieved note with ID: ${req.params.noteId}`,
-  });
-});
-
-// Middleware 404
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-// Middleware для обробки помилок
-app.use((err, req, res, next) => {
-  console.error(err);
-
-  const isProd = process.env.NODE_ENV === 'production';
-
-  res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.message,
-  });
-});
+// Error — якщо під час запиту виникла помилка
+app.use(errorHandler);
 
 // підключення до MongoDB
 await connectMongoDB();
