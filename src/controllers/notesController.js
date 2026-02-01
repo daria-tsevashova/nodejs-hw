255ac413dffddc311ce2ed355eaa52525c4aeb95
Dari <dariatsevashova@gmail.com>
2026-02-01 13:35:46 +0100

    add pagination, filtering and validation

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,8 +2,64 @@ import Note from '../models/note.js';
 import createHttpError from 'http-errors';
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find({});
-  res.status(200).json(notes);
+  const { tag, search, page = 1, perPage = 10 } = req.query;
+  const filter = {};
+
+  // Фільтрування за тегом
+  if (tag) {
+    filter.tag = tag;
+  }
+
+  // Текстовий пошук по title та content
+  if (search) {
+    filter.$text = { $search: search };
+  }
+
+  const skip = (page - 1) * perPage;
+
+  // Виконуємо одразу два запити паралельно
+  const [totalNotes, notes] = await Promise.all([
+    Note.find(filter).countDocuments(),
+    Note.find(filter).skip(skip).limit(perPage),
+  ]);
+
+  // Обчислюємо загальну кількість «сторінок»
+  const totalPages = Math.ceil(totalNotes / perPage);
+
+  res.status(200).json({
+    page: Number(page),
+    perPage: Number(perPage),
+    totalNotes,
+    totalPages,
+    notes,
+  });
+};
+
+export const getNotes = async (req, res) => {
+  // Отримуємо пара метри пагінації
+  const { page = 1, perPage = 12 } = req.query;
+
+  const skip = (page - 1) * perPage;
+
+  // Створюємо базовий запит до колекції
+  const notesQuery = Note.find();
+
+  // Виконуємо одразу два запити паралельно
+  const [totalItems, notes] = await Promise.all([
+    notesQuery.clone().countDocuments(),
+    notesQuery.skip(skip).limit(perPage),
+  ]);
+
+  // Обчислюємо загальну кількість «сторінок»
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalItems,
+    totalPages,
+    notes,
+  });
 };
 
 export const createNote = async (req, res) => {
--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,3 +1,36 @@
-const getAllNotesSchema = {};
+import { Segments, Joi } from 'celebrate';
+import { TAGS } from '../constants/tags.js';
 
-export { getAllNotesSchema };
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    tag: Joi.string().valid(...TAGS),
+    search: Joi.string().allow(''),
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+  }),
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().hex().length(24).required(),
+  }),
+};
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS),
+  }),
+};
+
+export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().hex().length(24).required(),
+  }),
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS),
+  }).min(1),
+};
